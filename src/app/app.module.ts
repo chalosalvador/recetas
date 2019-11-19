e4897455b77f8aea3ee2eb532e090af2320ffa0a
chalosalvador <chalosalvador@gmail.com>
2019-11-18 23:14:37 -0500

    #5 Estructura base de proyecto - Tabs - Conexión a Firebase - Login passwordless - Dynamic links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,35 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireFunctionsModule } from '@angular/fire/functions';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { FirebaseDynamicLinks } from '@ionic-native/firebase-dynamic-links/ngx';
 
-@NgModule({
-  declarations: [AppComponent],
+@NgModule( {
+  declarations: [ AppComponent ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [ BrowserModule,
+             IonicModule.forRoot(),
+             AppRoutingModule,
+             AngularFireModule.initializeApp( environment.firebase ),
+             // imports firebase/firestore, only needed for database features.
+             // enablePersistence allows offline data (https://github.com/angular/angularfire/blob/master/docs/firestore/offline-data.md)
+             AngularFirestoreModule.enablePersistence(),
+             AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+             AngularFireFunctionsModule, // firebase cloud functions
+             AngularFireMessagingModule // notifications,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    FirebaseDynamicLinks
   ],
-  bootstrap: [AppComponent]
-})
-export class AppModule {}
+  bootstrap: [ AppComponent ]
+} )
+export class AppModule {
+}
--- src/environments/environment.model.ts
+++ src/environments/environment.model.ts
@@ -2,9 +2,19 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
+export interface EnvironmentModel {
+  production: boolean;
+  firebase: {
+    apiKey: string,
+    authDomain: string,
+    databaseURL: string,
+    projectId: string,
+    storageBucket: string,
+    messagingSenderId: string,
+    appId: string,
+    measurementId: string
+  };
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
@@ -13,4 +23,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
