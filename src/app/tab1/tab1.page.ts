30658637d806723d143cdd91cb1ec875637e7725
chalosalvador <chalosalvador@gmail.com>
2019-11-20 07:22:50 -0500

    #5 Corrección login y logout

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { NgForm } from '@angular/forms';
+import { CommonService } from '../services/common.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,15 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private authService: AuthService, public commonService: CommonService ) {}
+
+  async doLogout() {
+    try {
+      await this.authService.logout();
+    } catch ( e ) {
+      console.error( e );
+      await this.commonService.presentAlert( 'Error', 'No se pudo enviar el correo electrónico de inicio de sesión.' );
+    }
+  }
 
 }
